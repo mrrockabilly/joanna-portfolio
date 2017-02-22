a96f8ceaa2c4bd854474de5bffc374935b6152ea
jrrubalc <jason.ray.rubalcaba@gmail.com>
2017-02-21 17:11:32 -0800

    adds image opimization

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var concat = require('gulp-concat');
 var cleanCSS = require('gulp-clean-css');
 var imagemin = require('gulp-imagemin');
 var htmlmin = require('gulp-htmlmin');
+var imageResize = require('gulp-image-resize');
 
 gulp.task('minify', function () {
   return gulp.src('*.html')
@@ -17,24 +18,24 @@ gulp.task('compr-img', () =>
     .pipe(gulp.dest('dist/images'))
 );
 
-gulp.task('scripts', function () {
-  gulp.src('js/*.js')
-    // .pipe(concat('all.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/js'))
-})
-
 gulp.task('minify-css', function () {
   return gulp.src('*.css')
     .pipe(cleanCSS({ compatibility: 'ie8' }))
     .pipe(gulp.dest('dist'));
 });
 
+gulp.task('resize', function () {
+  gulp.src('images/*')
+    .pipe(imageResize({
+      width : 1500,
+      upscale : false
+    }))
+    .pipe(gulp.dest('dist/cropped'));
+});
+
 gulp.task('default', function () {
-  gulp.run('minify', 'compr-img', 'scripts', 'minify-css');
-  gulp.task('watch', function () {
-    gulp.watch('js/*.js', ['scripts'])
-  })
+  gulp.run('minify', 'compr-img', 'minify-css');
+
   gulp.watch('*.css', function (event) {
     gulp.run('minify-css');
   })
